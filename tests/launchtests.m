1e80f437f00d515b1e9ddfc77904027685c5c825
Andrew Janke <andrew@apjanke.net>
2021-01-16 23:25:22 -0500

    Fix FAQ net link

--- tests/launchtests.m
+++
@@ -1,9 +0,0 @@
-function launchtests
-% Entry point for running full test suite from command line or automation
-
-rootdir = fileparts(fileparts(mfilename('fullpath')));
-addpath(fullfile(rootdir, 'Mcode'))
-
-results = mypackage.test.run_all_tests
-
-end
