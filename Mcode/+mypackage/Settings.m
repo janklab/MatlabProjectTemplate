4144db83aa829a3ac0898326c67e317af940e264
Andrew Janke <andrew@apjanke.net>
2021-09-11 11:13:43 -0400

    Helptext in Settings: fix missing ".".

--- Mcode/+mypackage/Settings.m
+++ Mcode/+mypackage/Settings.m
@@ -2,7 +2,7 @@ classdef Settings < mypackage.internal.MypackageBaseHandle
 % Global settings for the mypackage package
 %
 % Don't use this class directly. If you want to get or set the settings,
-% work with the instance of this in the mypackage.globals.settings field
+% work with the instance of this in the mypackage.globals.settings field.
 
   properties
   end
