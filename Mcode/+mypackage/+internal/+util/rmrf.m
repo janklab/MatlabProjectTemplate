16b5b555946db7768b85c1b3bfc8805c46fa3542
Andrew Janke <andrew@apjanke.net>
2021-09-11 23:48:05 -0400

    Fix rmrf logic to correclty handle directories

--- Mcode/+mypackage/+internal/+util/rmrf.m
+++ Mcode/+mypackage/+internal/+util/rmrf.m
@@ -2,27 +2,43 @@ function rmrf(files)
 % Recursively delete files and directories
 %
 % rmrf(files)
+%
+% files is a list of files and directories to delete. They may contain "*"
+% wildcards.
+%
+% Nonexistent directories and non-matching patterns are silently ignored.
 arguments
   files string
 end
 
 %#ok<*STRNU>
 
-RAII.warn = mypackage.internal.util.withwarnoff('MATLAB:DELETE:FileNotFound');
+RAII.warn = withwarnoff('MATLAB:DELETE:FileNotFound');
+
 % Sigh. We have to glob out the files ourselves because Matlab doesn't have a
 % delete operation that will work on both files and directories.
 for iGlob = 1:numel(files)
-  [~,details] = mypackage.internal.util.dir2(files(iGlob));
-  paths = details.path;
-  for file = paths'
-    if isfile(file)
-      delete(file);
-    elseif isfolder(file)
-      mypackage.internal.util.rmdir2(file, 's');
-    else
-      warning('Dir result "%s" is neither a file nor folder. Huh???', file);
+  glob = files(iGlob);
+  if glob.contains("*")
+    [~,details] = mypackage.internal.util.dir2(files(iGlob));
+    paths = details.path;
+    for file = paths'
+      rmrf_one_file(file);
     end
+  else
+    file = glob;
+    rmrf_one_file(file);
   end
 end
 
+end
+
+function rmrf_one_file(file)
+if isfile(file)
+  delete(file);
+elseif isfolder(file)
+  mypackage.internal.util.rmdir2(file, 's');
+else
+  % Ignore nonexistent files.
+end
 end
--- dev-kit/private/rmrf.m
+++ dev-kit/private/rmrf.m
@@ -9,20 +9,31 @@ end
 %#ok<*STRNU>
 
 RAII.warn = withwarnoff('MATLAB:DELETE:FileNotFound');
+
 % Sigh. We have to glob out the files ourselves because Matlab doesn't have a
 % delete operation that will work on both files and directories.
 for iGlob = 1:numel(files)
-  [~,details] = dir2(files(iGlob));
-  paths = details.path;
-  for file = paths'
-    if isfile(file)
-      delete(file);
-    elseif isfolder(file)
-      rmdir2(file, 's');
-    else
-      warning('Dir result "%s" is neither a file nor folder. Huh???', file);
+  glob = files(iGlob);
+  if glob.contains("*")
+    [~,details] = dir2(files(iGlob));
+    paths = details.path;
+    for file = paths'
+      rmrf_one_file(file);
     end
+  else
+    file = glob;
+    rmrf_one_file(file);
   end
 end
 
+end
+
+function rmrf_one_file(file)
+if isfile(file)
+  delete(file);
+elseif isfolder(file)
+  rmdir2(file, 's');
+else
+  % Ignore nonexistent files.
+end
 end
